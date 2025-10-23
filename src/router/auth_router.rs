592108fd57d93a7850b41c3965d105b4b02b5702
bio_tngiabao <bao.thai@bioturing.com>
2025-10-23 09:59:47 +0700

    chore: comment out unused code in router modules

--- src/router/auth_router.rs
+++ src/router/auth_router.rs
@@ -1,142 +1,142 @@
-use axum::{ Json, extract::{ FromRequest, Extension }, http::StatusCode };
-use bcrypt::{ bcrypt, verify, DEFAULT_COST };
-use sea_orm::{ ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter };
-use serde::de::DeserializeOwned;
-use serde::{ Deserialize, Serialize };
-use validator::Validate;
-use crate::dto::response::ApiResponse;
-use sea_orm::DatabaseConnection;
+// use axum::{ Json, extract::{ FromRequest, Extension }, http::StatusCode };
+// use bcrypt::{ bcrypt, verify, DEFAULT_COST };
+// use sea_orm::{ ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter };
+// use serde::de::DeserializeOwned;
+// use serde::{ Deserialize, Serialize };
+// use validator::Validate;
+// use crate::dto::response::ApiResponse;
+// use sea_orm::DatabaseConnection;
 
-use crate::entities::users;
+// use crate::entities::users;
 
-#[derive(Clone, Deserialize, Validate)]
-pub struct RegisterDto {
-    #[validate(length(min = 3, message = "username must be at least 3 characters"))]
-    username: String,
-    #[validate(
-        length(min = 3, message = "email must be at least 3 characters"),
-        email(message = "email must be a valid email address")
-    )]
-    email: String,
-    #[validate(length(min = 6, message = "password must be at least 6 characters"))]
-    password: String,
-}
+// #[derive(Clone, Deserialize, Validate)]
+// pub struct RegisterDto {
+//     #[validate(length(min = 3, message = "username must be at least 3 characters"))]
+//     username: String,
+//     #[validate(
+//         length(min = 3, message = "email must be at least 3 characters"),
+//         email(message = "email must be a valid email address")
+//     )]
+//     email: String,
+//     #[validate(length(min = 6, message = "password must be at least 6 characters"))]
+//     password: String,
+// }
 
-#[derive(Clone, Serialize)]
-pub struct UserResponse {
-    id: i32,
-    username: String,
-    email: String,
-}
+// #[derive(Clone, Serialize)]
+// pub struct UserResponse {
+//     id: i32,
+//     username: String,
+//     email: String,
+// }
 
-pub struct ValidatedJson<T>(pub T);
-impl<B, T> FromRequest<B>
-    for ValidatedJson<T>
-    where T: DeserializeOwned + Validate + Send, B: Send + Sync
-{
-    type Rejection = (StatusCode, Json<ApiResponse<()>>);
-    async fn from_request(req: axum::extract::Request, state: &B) -> Result<Self, Self::Rejection> {
-        let Json(value) = Json::<T>
-            ::from_request(req, state).await
-            .map_err(|e| ApiResponse::error(&e.to_string(), StatusCode::BAD_REQUEST))?;
+// pub struct ValidatedJson<T>(pub T);
+// impl<B, T> FromRequest<B>
+//     for ValidatedJson<T>
+//     where T: DeserializeOwned + Validate + Send, B: Send + Sync
+// {
+//     type Rejection = (StatusCode, Json<ApiResponse<()>>);
+//     async fn from_request(req: axum::extract::Request, state: &B) -> Result<Self, Self::Rejection> {
+//         let Json(value) = Json::<T>
+//             ::from_request(req, state).await
+//             .map_err(|e| ApiResponse::error(&e.to_string(), StatusCode::BAD_REQUEST))?;
 
-        value
-            .validate()
-            .map_err(|e|
-                ApiResponse::<()>::error(
-                    &format!("Validation failed: {e}"),
-                    StatusCode::BAD_REQUEST
-                )
-            )?;
+//         value
+//             .validate()
+//             .map_err(|e|
+//                 ApiResponse::<()>::error(
+//                     &format!("Validation failed: {e}"),
+//                     StatusCode::BAD_REQUEST
+//                 )
+//             )?;
 
-        Ok(ValidatedJson(value))
-    }
-}
+//         Ok(ValidatedJson(value))
+//     }
+// }
 
-pub async fn register(
-    Extension(db): Extension<DatabaseConnection>,
-    ValidatedJson(payload): ValidatedJson<RegisterDto>
-) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
-    let find_user = users::Entity
-        ::find()
-        .filter(users::Column::Email.eq(payload.email.clone()))
-        .one(&db).await;
+// pub async fn register(
+//     Extension(db): Extension<DatabaseConnection>,
+//     ValidatedJson(payload): ValidatedJson<RegisterDto>
+// ) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
+//     let find_user = users::Entity
+//         ::find()
+//         .filter(users::Column::Email.eq(payload.email.clone()))
+//         .one(&db).await;
 
-    if let Ok(Some(_)) = find_user {
-        return ApiResponse::error("Email already in use", StatusCode::CONFLICT);
-    }
+//     if let Ok(Some(_)) = find_user {
+//         return ApiResponse::error("Email already in use", StatusCode::CONFLICT);
+//     }
 
-    // hash password
-    let hash_password = match bcrypt::hash(payload.password, DEFAULT_COST) {
-        Ok(hash) => hash,
-        Err(err) => {
-            return ApiResponse::error(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR);
-        }
-    };
+//     // hash password
+//     let hash_password = match bcrypt::hash(payload.password, DEFAULT_COST) {
+//         Ok(hash) => hash,
+//         Err(err) => {
+//             return ApiResponse::error(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR);
+//         }
+//     };
 
-    let new_user = users::ActiveModel {
-        username: sea_orm::Set(payload.username.clone()),
-        email: sea_orm::Set(payload.email.clone()),
-        password: sea_orm::Set(hash_password),
-        ..Default::default()
-    };
+//     let new_user = users::ActiveModel {
+//         username: sea_orm::Set(payload.username.clone()),
+//         email: sea_orm::Set(payload.email.clone()),
+//         password: sea_orm::Set(hash_password),
+//         ..Default::default()
+//     };
 
-    return match new_user.insert(&db).await {
-        Ok(user) => {
-            let user_response = UserResponse {
-                id: user.id as i32,
-                username: user.username,
-                email: user.email,
-            };
-            ApiResponse::success(user_response, "User registered successfully")
-        }
-        Err(err) =>
-            ApiResponse::error(
-                &format!("Failed to register user: {}", err),
-                StatusCode::INTERNAL_SERVER_ERROR
-            ),
-    };
-}
+//     return match new_user.insert(&db).await {
+//         Ok(user) => {
+//             let user_response = UserResponse {
+//                 id: user.id as i32,
+//                 username: user.username,
+//                 email: user.email,
+//             };
+//             ApiResponse::success(user_response, "User registered successfully")
+//         }
+//         Err(err) =>
+//             ApiResponse::error(
+//                 &format!("Failed to register user: {}", err),
+//                 StatusCode::INTERNAL_SERVER_ERROR
+//             ),
+//     };
+// }
 
-#[derive(Clone, Deserialize, Validate)]
-pub struct LoginDto {
-    #[validate(length(min = 3, message = "email must be at least 3 characters"))]
-    pub email: String,
-    #[validate(length(min = 6, message = "password must be at least 6 characters"))]
-    pub password: String,
-}
+// #[derive(Clone, Deserialize, Validate)]
+// pub struct LoginDto {
+//     #[validate(length(min = 3, message = "email must be at least 3 characters"))]
+//     pub email: String,
+//     #[validate(length(min = 6, message = "password must be at least 6 characters"))]
+//     pub password: String,
+// }
 
-pub async fn login(
-    Extension(db): Extension<DatabaseConnection>,
-    ValidatedJson(payload): ValidatedJson<LoginDto>
-) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
-    let result = users::Entity
-        ::find()
-        .filter(users::Column::Email.eq(payload.email.clone()))
-        .one(&db).await;
+// pub async fn login(
+//     Extension(db): Extension<DatabaseConnection>,
+//     ValidatedJson(payload): ValidatedJson<LoginDto>
+// ) -> (StatusCode, Json<ApiResponse<UserResponse>>) {
+//     let result = users::Entity
+//         ::find()
+//         .filter(users::Column::Email.eq(payload.email.clone()))
+//         .one(&db).await;
 
-    let find_user = match result {
-        Ok(Some(user)) => user,
-        Ok(None) => {
-            return ApiResponse::error("User not found", StatusCode::NOT_FOUND);
-        }
-        Err(err) => {
-            return ApiResponse::error(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR);
-        }
-    };
+//     let find_user = match result {
+//         Ok(Some(user)) => user,
+//         Ok(None) => {
+//             return ApiResponse::error("User not found", StatusCode::NOT_FOUND);
+//         }
+//         Err(err) => {
+//             return ApiResponse::error(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR);
+//         }
+//     };
 
-    let is_valid = verify(payload.password, &find_user.password).unwrap_or(false);
+//     let is_valid = verify(payload.password, &find_user.password).unwrap_or(false);
 
-    if !is_valid {
-        return ApiResponse::error("Incorect password", StatusCode::UNAUTHORIZED);
-    }
+//     if !is_valid {
+//         return ApiResponse::error("Incorect password", StatusCode::UNAUTHORIZED);
+//     }
 
-    return ApiResponse::success(
-        UserResponse {
-            id: find_user.id,
-            username: find_user.username,
-            email: find_user.email, // assuming you added it to UserResponse
-        },
-        "Login"
-    );
-}
+//     return ApiResponse::success(
+//         UserResponse {
+//             id: find_user.id,
+//             username: find_user.username,
+//             email: find_user.email, // assuming you added it to UserResponse
+//         },
+//         "Login"
+//     );
+// }
--- src/router/task_router.rs
+++ src/router/task_router.rs
@@ -1,97 +1,97 @@
-use std::{ os::macos::raw::stat, task };
+// use std::{ os::macos::raw::stat, task };
 
-use axum::{ extract::Path, http::{ header, StatusCode }, Extension, Json };
-use sea_orm::{ ActiveModelTrait, DatabaseConnection, EntityTrait, Set };
-use serde::{ Deserialize, Serialize };
-use validator::Validate;
+// use axum::{ extract::Path, http::{ header, StatusCode }, Extension, Json };
+// use sea_orm::{ ActiveModelTrait, DatabaseConnection, EntityTrait, Set };
+// use serde::{ Deserialize, Serialize };
+// use validator::Validate;
 
-use crate::{ dto::response::ApiResponse, entities::tasks, router::auth_router::ValidatedJson };
-use crate::dto::response;
+// use crate::{ dto::response::ApiResponse, entities::tasks, router::auth_router::ValidatedJson };
+// use crate::dto::response;
 
-#[derive(Deserialize, Serialize, Validate)]
-pub struct TaskCreated {
-    pub priority: Option<String>,
-    pub title: String,
-    pub description: Option<String>,
-    pub user_id: i32,
-    pub is_default: Option<bool>,
-}
+// #[derive(Deserialize, Serialize, Validate)]
+// pub struct TaskCreated {
+//     pub priority: Option<String>,
+//     pub title: String,
+//     pub description: Option<String>,
+//     pub user_id: i32,
+//     pub is_default: Option<bool>,
+// }
 
-pub async fn create_task(
-    Extension(db): Extension<DatabaseConnection>,
-    ValidatedJson(payload): ValidatedJson<TaskCreated>
-) -> String {
-    let new_task = tasks::ActiveModel {
-        priority: Set(payload.priority),
-        title: Set(payload.title),
-        description: Set(payload.description),
-        user_id: Set(Some(payload.user_id)),
-        is_default: Set(payload.is_default),
-        ..Default::default()
-    };
+// pub async fn create_task(
+//     Extension(db): Extension<DatabaseConnection>,
+//     ValidatedJson(payload): ValidatedJson<TaskCreated>
+// ) -> String {
+//     let new_task = tasks::ActiveModel {
+//         priority: Set(payload.priority),
+//         title: Set(payload.title),
+//         description: Set(payload.description),
+//         user_id: Set(Some(payload.user_id)),
+//         is_default: Set(payload.is_default),
+//         ..Default::default()
+//     };
 
-    let result = new_task.insert(&db).await;
-    match result {
-        Ok(task) => format!("✅ Task created with ID: {}", task.id),
-        Err(err) => format!("❌ Failed to create task: {}", err),
-    }
-}
+//     let result = new_task.insert(&db).await;
+//     match result {
+//         Ok(task) => format!("✅ Task created with ID: {}", task.id),
+//         Err(err) => format!("❌ Failed to create task: {}", err),
+//     }
+// }
 
-#[derive(Serialize)]
-pub struct TaskResponse {
-    pub id: i32,
-    pub priority: Option<String>,
-    pub title: String,
-    pub description: Option<String>,
-    pub user_id: i32,
-    pub is_default: Option<bool>,
-}
+// #[derive(Serialize)]
+// pub struct TaskResponse {
+//     pub id: i32,
+//     pub priority: Option<String>,
+//     pub title: String,
+//     pub description: Option<String>,
+//     pub user_id: i32,
+//     pub is_default: Option<bool>,
+// }
 
-pub async fn get_task_by_id(
-    Extension(db): Extension<DatabaseConnection>,
-    Path(id): Path<i32>
-) -> (StatusCode, Json<ApiResponse<TaskResponse>>) {
-    print!("Fetching task with ID: {}", id);
-    match tasks::Entity::find_by_id(id).one(&db).await {
-        Ok(Some(task)) =>
-            ApiResponse::success(
-                TaskResponse {
-                    id: task.id,
-                    title: task.title,
-                    priority: task.priority,
-                    description: task.description,
-                    user_id: task.user_id.unwrap_or(0),
-                    is_default: task.is_default,
-                },
-                "Task retrieved successfully"
-            ),
+// pub async fn get_task_by_id(
+//     Extension(db): Extension<DatabaseConnection>,
+//     Path(id): Path<i32>
+// ) -> (StatusCode, Json<ApiResponse<TaskResponse>>) {
+//     print!("Fetching task with ID: {}", id);
+//     match tasks::Entity::find_by_id(id).one(&db).await {
+//         Ok(Some(task)) =>
+//             ApiResponse::success(
+//                 TaskResponse {
+//                     id: task.id,
+//                     title: task.title,
+//                     priority: task.priority,
+//                     description: task.description,
+//                     user_id: task.user_id.unwrap_or(0),
+//                     is_default: task.is_default,
+//                 },
+//                 "Task retrieved successfully"
+//             ),
 
-        Ok(None) => ApiResponse::error("Task not found", StatusCode::NOT_FOUND),
-        Err(_) => ApiResponse::error("Internal server error", StatusCode::INTERNAL_SERVER_ERROR),
-    }
-}
+//         Ok(None) => ApiResponse::error("Task not found", StatusCode::NOT_FOUND),
+//         Err(_) => ApiResponse::error("Internal server error", StatusCode::INTERNAL_SERVER_ERROR),
+//     }
+// }
 
-pub async fn get_all_tasks(Extension(db): Extension<DatabaseConnection>) -> (
-    StatusCode,
-    Json<ApiResponse<Vec<TaskResponse>>>,
-) {
-    match tasks::Entity::find().all(&db).await {
-        Ok(task_list) => {
-            let data: Vec<TaskResponse> = task_list
-                .into_iter()
-                .map(|task| {
-                    TaskResponse {
-                        id: task.id,
-                        title: task.title,
-                        priority: task.priority,
-                        description: task.description,
-                        user_id: task.user_id.unwrap_or(0),
-                        is_default: task.is_default,
-                    }
-                })
-                .collect();
-            ApiResponse::success(data, "Tasks retrieved successfully")
-        }
-        Err(_) => ApiResponse::error("Internal server error", StatusCode::INTERNAL_SERVER_ERROR),
-    }
-}
+// pub async fn get_all_tasks(Extension(db): Extension<DatabaseConnection>) -> (
+//     StatusCode,
+//     Json<ApiResponse<Vec<TaskResponse>>>,
+// ) {
+//     match tasks::Entity::find().all(&db).await {
+//         Ok(task_list) => {
+//             let data: Vec<TaskResponse> = task_list
+//                 .into_iter()
+//                 .map(|task| {
+//                     TaskResponse {
+//                         id: task.id,
+//                         title: task.title,
+//                         priority: task.priority,
+//                         description: task.description,
+//                         user_id: task.user_id.unwrap_or(0),
+//                         is_default: task.is_default,
+//                     }
+//                 })
+//                 .collect();
+//             ApiResponse::success(data, "Tasks retrieved successfully")
+//         }
+//         Err(_) => ApiResponse::error("Internal server error", StatusCode::INTERNAL_SERVER_ERROR),
+//     }
+// }
